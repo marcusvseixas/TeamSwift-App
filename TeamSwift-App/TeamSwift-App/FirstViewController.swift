4cb7263651f3f362c05e5aaf1dccea8436de3692
Marcus Vinicius Seixas <mvssk8@yahoo.com.br>
2016-02-11 18:04:18 -0500

    First Commit

---
+++ TeamSwift-App/TeamSwift-App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TeamSwift-App
+//
+//  Created by Marcus Vinicius Seixas on 11/02/16.
+//
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
