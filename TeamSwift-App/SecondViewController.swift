42a5dacb65653e7627fde97bb9c01bcac693b169
sjw787 <swylock@albany.edu>
2016-03-03 03:11:50 -0500

    Updated the add food screen. Now you can tap scan food and it will open the scanner.

--- TeamSwift-App/SecondViewController.swift
+++ TeamSwift-App/SecondViewController.swift
@@ -12,61 +12,68 @@ class SecondViewController: UIViewController, AVCaptureMetadataOutputObjectsDele
     var captureSession:AVCaptureSession?
     var videoPreviewLayer:AVCaptureVideoPreviewLayer?
     var qrCodeFrameView:UIView?
+    var isPressed = false
     
+    @IBAction func buttonPressed(sender: AnyObject) {
+        isPressed = true
+        viewDidLoad()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video
         // as the media type parameter.
-
-        do {
-            // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video
-            // as the media type parameter.
-            let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
-            let input = try AVCaptureDeviceInput(device: captureDevice) // Get an instance of the AVCaptureDeviceInput class using the previous device object.
-            captureSession = AVCaptureSession()                         // Initialize the captureSession object.
-            captureSession?.addInput(input as AVCaptureInput)           // Add and input to it
+        if(isPressed ==  true){
+    
+            do {
+                // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video
+                // as the media type parameter.
+                let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+                let input = try AVCaptureDeviceInput(device: captureDevice) // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+                captureSession = AVCaptureSession()                         // Initialize the captureSession object.
+                captureSession?.addInput(input as AVCaptureInput)           // Add and input to it
            
-        } catch let error as NSError {                                  // Catch any errors that arise from this like no camera detected
-            print(error)
-        }
+            } catch let error as NSError {                                  // Catch any errors that arise from this like no camera detected
+                print(error)
+            }
         
-        // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
         
-        let captureMetadataOutput = AVCaptureMetadataOutput()
+            let captureMetadataOutput = AVCaptureMetadataOutput()
         
-        // Tests to see if the device has a camera or not
-        if ((captureSession?.canAddOutput(captureMetadataOutput)) != nil) {
-            captureSession?.addOutput(captureMetadataOutput)
+            // Tests to see if the device has a camera or not
+            if ((captureSession?.canAddOutput(captureMetadataOutput)) != nil) {
+                captureSession?.addOutput(captureMetadataOutput)
         
-            // Set delegate and use the default dispatch queue to execute the call back
-            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
-            // These are all of the types of codes that the device can scan
-            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode,AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeUPCECode,AVMetadataObjectTypeCode128Code]
+                // Set delegate and use the default dispatch queue to execute the call back
+                captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+                // These are all of the types of codes that the device can scan
+                captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode,AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeUPCECode,AVMetadataObjectTypeCode128Code]
   
-            // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
-            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
-            videoPreviewLayer?.frame = view.layer.bounds
-            view.layer.addSublayer(videoPreviewLayer!)
+                // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+                videoPreviewLayer?.frame = view.layer.bounds
+                view.layer.addSublayer(videoPreviewLayer!)
         
-            // Start video capture.
-            captureSession?.startRunning()
+                // Start video capture.
+                captureSession?.startRunning()
         
-            // Move the message label to the top view
-            view.bringSubviewToFront(messageLabel)
+                // Move the message label to the top view
+                view.bringSubviewToFront(messageLabel)
         
-            // Initialize QR Code Frame to highlight the QR code
-            qrCodeFrameView = UIView()
-            qrCodeFrameView?.layer.borderColor = UIColor.greenColor().CGColor
-            qrCodeFrameView?.layer.borderWidth = 2
-            view.addSubview(qrCodeFrameView!)
-            view.bringSubviewToFront(qrCodeFrameView!)
-        }
-        else{ // If device has no camera or camera cannot be detected, then display a message
-            messageLabel.text = "No Camera Detected, Cannot Scan"
-            return
+                // Initialize QR Code Frame to highlight the QR code
+                qrCodeFrameView = UIView()
+                qrCodeFrameView?.layer.borderColor = UIColor.greenColor().CGColor
+                qrCodeFrameView?.layer.borderWidth = 2
+                view.addSubview(qrCodeFrameView!)
+                view.bringSubviewToFront(qrCodeFrameView!)
+            }
+            else{ // If device has no camera or camera cannot be detected, then display a message
+                messageLabel.text = "No Camera Detected, Cannot Scan"
+                return
+            }
         }
     }
     
